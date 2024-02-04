8a743066241c1175b486e3091bcca0dc3c575865
AtakanTurgut <77547962+AtakanTurgut@users.noreply.github.com>
2024-02-04 16:15:01 +0300

    Admin theme has created.

---
+++ SignalFood/SignalFoodWebUI/ViewComponents/LayoutComponents/_LayoutScriptPartialComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalFoodWebUI.ViewComponents.LayoutComponents
+{
+    public class _LayoutScriptPartialComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
