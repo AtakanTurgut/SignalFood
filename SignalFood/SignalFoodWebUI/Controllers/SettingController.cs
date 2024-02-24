28ea3793966a1a5c453b58d9d25617067a1f0551
AtakanTurgut <77547962+AtakanTurgut@users.noreply.github.com>
2024-02-24 21:15:55 +0300

    UserEdit has been completed.

---
+++ SignalFood/SignalFoodWebUI/Controllers/SettingController.cs
@@ -0,0 +1,51 @@
+﻿using EntityLayer.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SignalFoodWebUI.Dtos.IdentityDtos;
+
+namespace SignalFoodWebUI.Controllers
+{
+    public class SettingController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public SettingController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            UserEditDto userEditDto = new UserEditDto();
+            userEditDto.Name = values.Name;
+            userEditDto.Surname = values.Surname;
+            userEditDto.Username = values.UserName;
+            userEditDto.Mail = values.Email;
+
+            return View(userEditDto);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(UserEditDto userEditDto)
+        {
+            if (userEditDto.Password == userEditDto.ConfirmPassword)
+            {
+                var user = await _userManager.FindByNameAsync(User.Identity.Name);
+                user.Name = userEditDto.Name;
+                user.Surname = userEditDto.Surname;
+                user.UserName = userEditDto.Username;
+                user.Email = userEditDto.Mail;
+                user.PasswordHash = _userManager.PasswordHasher.HashPassword(user, userEditDto.Password);
+
+                await _userManager.UpdateAsync(user);
+
+                return RedirectToAction("Index", "Statistic");
+            }
+
+            return View();
+        }
+
+    }
+}
